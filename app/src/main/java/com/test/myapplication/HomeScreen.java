999642ce358f3b2213c4a693a1d3ee6fb6274d30
hoangphuc552001 <67599931+hoangphuc552001@users.noreply.github.com>
2021-07-03 19:23:48 +0700

    logouser
    logouser

--- app/src/main/java/com/test/myapplication/HomeScreen.java
+++ app/src/main/java/com/test/myapplication/HomeScreen.java
@@ -2,13 +2,24 @@ package com.test.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
-
+    Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        btnLogout=findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRegisterScreen = new Intent(HomeScreen.this, MainActivity.class);
+                startActivity(goToRegisterScreen);
+            }
+        });
     }
 }
